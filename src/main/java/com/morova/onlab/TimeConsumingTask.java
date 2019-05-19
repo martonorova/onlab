40ebc6f60c62cbebffb35626505b981c1d7aee85
martonorova <orova.marton@gmail.com>
2019-05-19 16:06:26 +0200

    Update TimeConsumingTask.java

--- src/main/java/com/morova/onlab/TimeConsumingTask.java
+++ src/main/java/com/morova/onlab/TimeConsumingTask.java
@@ -3,27 +3,20 @@ package com.morova.onlab;
 import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class TimeConsumingTask implements Runnable {
-
     private static int counter = 1;
     private int id;
-
     public TimeConsumingTask() {
         this.id = counter++;
     }
 
     @Override
     public void run() {
-
         NormalDistribution normalDistribution = new NormalDistribution(10000, 5000);
         long millisToSleep = (long) normalDistribution.sample();
         if (millisToSleep < 0) {
             millisToSleep *= -1;
         }
-
-       // System.out.println("MILLIS TO SLEEP = " + millisToSleep);
-
         try {
-            //System.out.println("START RUNnable " + id + " at: " + System.currentTimeMillis() % 100000);
             Thread.sleep(millisToSleep);
         } catch (InterruptedException e) {
             e.printStackTrace();
